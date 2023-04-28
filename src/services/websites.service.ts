e9b1e9f9ec41eca8ef1ea0c0d2d6ab0575b1d2c7
AndreyNovaes <andreynovaespro@gmail.com>
2023-04-27 23:16:06 -0300

    fix: trying debug aws

--- src/services/websites.service.ts
+++ src/services/websites.service.ts
@@ -1,19 +1,19 @@
 import { poolConnectionClient } from "../database/pgPoolConnection.database";
-import { redisClient } from "../database/redisClient";
+// import { redisClient } from "../database/redisClient";
 
 export async function getWebsitesService() {
   const query = "SELECT DISTINCT website FROM scrapped_data";
   const cacheKey = "websites";
   try {
-    const cachedData = await redisClient.get(cacheKey);
-    if (cachedData) {
-      return JSON.parse(cachedData);
-    }
+    // const cachedData = await redisClient.get(cacheKey);
+    // if (cachedData) {
+    //   return JSON.parse(cachedData);
+    // }
     const { rows } = await poolConnectionClient.query(query);
     const websites = rows.map((row) => row.website);
 
-    const secondsInADay = 86400;
-    await redisClient.setex(cacheKey, secondsInADay, JSON.stringify(websites));
+    // const secondsInADay = 86400;
+    // await redisClient.setex(cacheKey, secondsInADay, JSON.stringify(websites));
     
     return websites;
   } catch (error) {
