998fa3b286942b9c658c711a1d01b70ca8069e82
AndreyNovaes <andreynovaespro@gmail.com>
2023-05-04 21:57:18 -0300

    feat: add redis client and password on .env

--- src/database/redisClient.ts
+++ src/database/redisClient.ts
@@ -1,7 +1,10 @@
 import Redis from 'ioredis';
 
 const redisConnection = process.env.REDIS_CONNECTION || 'redis://localhost:6379';
+const redisPassword = process.env.REDIS_PASSWORD || '';
 
-const redisClient = new Redis( redisConnection );
+const redisClient = new Redis(redisConnection, {
+  password: redisPassword,
+});
 
 export { redisClient };
