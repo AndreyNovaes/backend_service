302ee4d1ce65d561db6db5026597fece31d11179
AndreyNovaes <andreynovaespro@gmail.com>
2023-05-04 21:27:31 -0300

    REFACTOR: change my routes and app to use SOLID and POO

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
-// routes
-import { categoriesRouter } from "./router/categories.route";
-import { websitesRouter } from "./router/website.route";
-import { searchRouter } from "./router/search.route";
-// validation middleware
+// controllers (dependency injection)
+import { categoriesController } from "./dependencyWiring/categories.wiring";
+import { websitesController } from "./dependencyWiring/websites.wiring";
+import { searchController } from "./dependencyWiring/search.wiring";
+// route creators
+import { createCategoriesRouter } from "./routes/categories.route";
+import { createWebsitesRouter } from "./routes/websites.route";
+import { createSearchRouter } from "./routes/search.route";
+// cors
 import cors from "cors";
-import { validateCategoryMiddleware } from "./middlewares/category.validation";
 // error middleware
 import { errorMiddleware } from "./Error/error.middleware";
-import { validateWebsiteMiddleware } from "./middlewares/website.validation";
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use("/search", validateCategoryMiddleware, validateWebsiteMiddleware, searchRouter);
-app.use("/categories", categoriesRouter);
-app.use("/websites", websitesRouter);
+
+app.use("/categories", createCategoriesRouter(categoriesController));
+app.use("/websites", createWebsitesRouter(websitesController));
+app.use("/search", createSearchRouter(searchController));
+
 app.use(errorMiddleware);
 
 export default app;
